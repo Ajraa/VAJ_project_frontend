a45982800942ddbe4f7189383c16c536be86290d
Pavel Salajka <pavel.salajka@avesoft.cz>
2024-04-23 15:32:27 +0200

    Deleted generated pages by Vite and initialized router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Login from "./login";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Login/>
+    }
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
